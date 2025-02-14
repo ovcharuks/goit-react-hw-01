2059cf8f9100151ab3a2d7b05bb0833c008623f5
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-02-14 18:41:39 +0200

    c

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,12 +1,12 @@
 import styles from "./FriendListItem.module.css";
 
-const FriendListItem = ({ avatar, name, isOnline, id }) => {
+const FriendListItem = ({ avatar, name, status, id }) => {
   return (
     <div className={styles.friendContainer}>
       <img src={avatar} alt={name} width="48" />
       <h4>{name}</h4>
-      <p className={isOnline ? styles.statusOnline : styles.statusIsOffline}>
-        {isOnline ? "Online" : "Offline"}
+      <p className={status ? styles.statusOnline : styles.statusIsOffline}>
+        {status ? "Online" : "Offline"}
       </p>
     </div>
   );
